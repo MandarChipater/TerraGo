04cfd5de5d984e4b0256126d73b32abbcff958ec
MandarChipater <80892089+MandarChipater@users.noreply.github.com>
2023-03-14 17:56:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,17 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraGo-test-network-01"
+resource "google_compute_instance" "vm_instance" {
+  name         = "gcptutorials-vm"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }    
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
 }
