e128dcd0a3eecf9658878d96b3673a42fe1f9259
MandarChipater <80892089+MandarChipater@users.noreply.github.com>
2023-03-14 18:21:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "Debian GNU/Linux 11 (bullseye)"
     }
   }    
   network_interface {
